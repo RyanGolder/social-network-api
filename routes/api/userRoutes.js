467b14de0768ee42f86b2ed25cbe66b0cb560931
Ryan Golder <rgolder78@gmail.com>
2023-06-21 20:19:27 +1000

    Updates

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,17 +1,18 @@
 // API Routes
 
 const router = require('express').Router();
-const User = require('../models/User');
-const Thought = require('../models/Thought');
+const User = require('../../models/User');
+const Thought = require('../../models/Thought');
 
 // /api/users
 
 // GET all users
 router.get('/users', async (req, res) => {
   try {
-    const users = await User.find().populate('thoughts').populate('friends');
+    const users = await User.find();
     res.json(users);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
